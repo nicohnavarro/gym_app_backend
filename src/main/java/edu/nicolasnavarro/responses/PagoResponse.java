95b857d92c77497b010c9328756faf377fb0d15b
nicohnavarro <nicohnavarro@gmail.com>
2020-08-24 00:11:52 -0300

    017-Se agrega pagos

--- src/main/java/edu/nicolasnavarro/responses/PagoResponse.java
+++ src/main/java/edu/nicolasnavarro/responses/PagoResponse.java
@@ -1,5 +1,34 @@
 package edu.nicolasnavarro.responses;
+import edu.nicolasnavarro.dao.PagoDTO;
 
-public class PagoResponse {
+public class PagoResponse implements EntitiesApplication<PagoDTO> {
 
+	private PagoDTO pago;
+	private String message;
+	private int statusCode;
+	
+	public PagoResponse(PagoDTO pago) {
+		this.pago=pago;
+	}
+	
+	public PagoDTO getEntity() {
+		return this.pago;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	
 }
--- src/main/java/edu/nicolasnavarro/rest/controller/PagoController.java
+++ src/main/java/edu/nicolasnavarro/rest/controller/PagoController.java
@@ -1,5 +1,30 @@
 package edu.nicolasnavarro.rest.controller;
 
-public class PagoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.nicolasnavarro.dao.PagoDTO;
+import edu.nicolasnavarro.dao.repositories.PagoDtoRepository;
+import io.swagger.annotations.ApiOperation;
+
+@ApiOperation(value="/api/pago",tags ="Pago Profile Controller")
+@CrossOrigin
+@RestController
+@RequestMapping("/api/pago")
+public class PagoController {
+	
+	@Autowired
+	private PagoDtoRepository alumnoRepository;
+	
+	@ApiOperation(value="Get All Pagos", response=Iterable.class)
+	@GetMapping(value="getAll")
+	public List<PagoDTO> getAllPagos(){
+		return alumnoRepository.findAll();
+	
+	}
 }
